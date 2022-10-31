1a8562328d0aa2eb8c48c3462f1a9e8861eb4e17
kpilszak <klaudia.pilszak@gmail.com>
2022-10-31 17:43:47 +0100

    Use Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,9 @@ import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import { StarWarsService } from './star-wars.service';
 import { LogService } from './log.service';
-import { CreateCharacterComponent } from './create-character/create-character.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-
-const routes: Routes = [
-  { path: 'characters', component: TabsComponent, children: [
-    { path: '', redirectTo: 'all' , pathMatch: 'full' },
-    { path: ':side', component: ListComponent }
-  ] },
-  { path: 'new-character', component: CreateCharacterComponent },
-  { path: '**', redirectTo: '/' },
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -28,14 +19,13 @@ const routes: Routes = [
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
